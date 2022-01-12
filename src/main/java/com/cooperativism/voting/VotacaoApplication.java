7bab941ddeaaaf80ffc8afae0ce77d82b044e1b5
Renan Luis Bianchini <renan.bianchini@southsystem.com.br>
2022-01-12 00:46:21 -0300

    Removido o RabbitAutoConfiguration do spring boot

--- src/main/java/com/cooperativism/voting/VotacaoApplication.java
+++ src/main/java/com/cooperativism/voting/VotacaoApplication.java
@@ -3,12 +3,13 @@ package com.cooperativism.voting;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @EnableMongoAuditing
 @EnableRabbit
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, RabbitAutoConfiguration.class })
 public class VotacaoApplication {
 
 	public static void main(String[] args) {
