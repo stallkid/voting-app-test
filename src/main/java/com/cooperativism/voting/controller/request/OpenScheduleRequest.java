7dea74b856e6af2aedcf055002c574da98e13d18
Renan Luis Bianchini <renan.bianchini@southsystem.com.br>
2022-01-13 16:18:15 -0300

    Alterado valor padrão no parametro de data do Swagger

--- src/main/java/com/cooperativism/voting/controller/request/OpenScheduleRequest.java
+++ src/main/java/com/cooperativism/voting/controller/request/OpenScheduleRequest.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiParam;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -12,7 +13,8 @@ import lombok.ToString;
 public class OpenScheduleRequest {
     @ApiModelProperty(notes = "ID da pauta", required = true)
     private String scheduleId;
-    @ApiModelProperty(notes = "Data de fechamento da Pauta")
-    @ApiParam(value = "Data padrão como vazio, adiciona 1 minuto na hora apartir de agora", required = true, defaultValue = "", allowEmptyValue = true)
+    @ApiModelProperty(notes = "Data de fechamento da Pauta", allowableValues = "yyyy-mm-dd hh:mm:ss")
+    @ApiParam(value = "Data padrão como vazio, adiciona 1 minuto na hora apartir de agora", required = true)
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private String endDate;
 }
