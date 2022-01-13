4ba3af50309411dedbc5395a4316fcd46195deb8
Renan Luis Bianchini <renan.bianchini@southsystem.com.br>
2022-01-13 15:10:01 -0300

    Otimizado os logs para somente quando for disparado o resultado das votações para a fila

--- src/main/java/com/cooperativism/voting/service/ScheduleResultServiceImpl.java
+++ src/main/java/com/cooperativism/voting/service/ScheduleResultServiceImpl.java
@@ -30,18 +30,16 @@ public class ScheduleResultServiceImpl implements ScheduleResultService {
     @Scheduled(fixedRate = 5000)
     public void sendVoteResultToTheQueue() {
         var schedules = repository.findAllClosedScheduleToSendToQueue();
-        log.info("Iniciando a verificação das Pautas fechadas");
         schedules.stream()
-                .forEach( schedule -> {
-                    if (checkDataToSendMesssage(schedule)) {
-                        var voteResultMessage = mapper.toVoteResultMessageResponse(schedule);
-                        log.info("Enviando para a fila a Pauta: {}", voteResultMessage);
-                        producer.send(voteResultMessage.toString());
-                        schedule.setStatus(true);
-                        repository.save(schedule);
-                    }
-                });
-        log.info("Verificação de Pautas fechadas finalizado");
+            .forEach( schedule -> {
+                if (checkDataToSendMesssage(schedule)) {
+                    var voteResultMessage = mapper.toVoteResultMessageResponse(schedule);
+                    log.info("Enviando para a fila a Pauta: {}", voteResultMessage);
+                    producer.send(voteResultMessage.toString());
+                    schedule.setStatus(true);
+                    repository.save(schedule);
+                }
+            });
     }
 
     private boolean checkDataToSendMesssage(final Schedule schedule) {
